689bcf12066d7d1d2aead864ed73fe0134c98cd3
Evan Harmony <evan_harmony@hotmail.com>
2024-10-15 12:38:21 -0400

    Adding tsconfig and updating cypress settings. Added a new blank spec file as well.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://demo.playwright.dev/todomvc',
+    specPattern: '**/*.cy.ts',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
