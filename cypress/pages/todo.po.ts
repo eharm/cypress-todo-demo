35a1fd8a5e9a280e6ca1cf7b5e5af08ad92e8f76
Evan Harmony <evan_harmony@hotmail.com>
2024-10-18 06:22:00 -0400

    Adding page object for test. Also added a new test which uses that page object

---
+++ cypress/pages/todo.po.ts
@@ -0,0 +1,47 @@
+export class Todo {
+    /**
+     * Will add either a single or multiple items to the todo list
+     * @param todo item(s) to add to todo list
+     */
+    static createTodo(...todo: string[]) {
+        cy.wrap(todo).each((t: string, i) => {
+            cy.get('.new-todo').type(t + '{enter}');
+
+            cy.getByDataTag('todo-item')
+                .should('have.length.at.least', i+1)
+        })
+    }
+
+    /**
+    * This will toggle a checkbox for one or multiple todo items. There is an assertion
+    * on each check to confirm the expected state before and after check/uncheck
+    * @param state The desired state of the checkbox
+    * @param todoText The text of the todo to be check/unchecked
+    */
+    static toggleTodo(state: 'check' | 'uncheck', ...todoText: string[]) {
+        cy.wrap(todoText).each((t: string) => {
+            cy.getByDataTag('todo-item')
+                .contains('label', t)
+                .siblings('input')
+                .as('curCbox')
+                .should('have.length', 1)
+                .should(
+                    state === 'check'
+                        ? 'not.be.checked'
+                        : 'be.checked'
+                ).then(($cbox) => {
+                    if (state === 'check') {
+                        cy.wrap($cbox).check();
+                    } else {
+                        cy.wrap($cbox).uncheck();
+                    }
+                });
+
+            cy.get('@curCbox').should(
+                state === 'check'
+                    ? 'be.checked'
+                    : 'not.be.checked'
+            )
+        })
+    }
+}
--- cypress/tests/todo-demo.cy.ts
+++ cypress/tests/todo-demo.cy.ts
@@ -1,4 +1,6 @@
-describe('Verification of free todo MVC Project', () => {
+import { Todo } from "../pages/todo.po";
+
+describe('Verification of todo MVC Project', () => {
     const todos = [
         'Write first Cypress test',
         'Write second Cypress test',
@@ -9,7 +11,7 @@ describe('Verification of free todo MVC Project', () => {
         cy.visit('/');
     })
 
-    it('Create todos', () => {
+    it('Verify creation of todos', () => {
         // Check header
         cy.get('header h1').should('contain.text', 'todos');
 
@@ -32,6 +34,44 @@ describe('Verification of free todo MVC Project', () => {
                 .should('match', new RegExp(`${i+1} item${i === 0 ? '' : 's'} left`))
         })
     })
+    
+    describe('Page functionality with existing todos', () => {
+        beforeEach(() => {
+            Todo.createTodo(...todos);
+        })
 
+        it('Verify check/uncheck functionality', () => {
+            // Confirm unchecked state
+            cy.getByDataTag('todo-item')
+                .first()
+                .as('firstTodo')
+                .should('not.have.class', 'completed')
+                .find('label')
+                .as('firstTodoLabel')
+                .should('contain.text', todos[0])
+            cy.getByDataTag('todo-count')
+                .should('contain.text', `${todos.length} items left`);
+            cy.get('.clear-completed').should('not.exist');
 
+            // Check first todo
+            Todo.toggleTodo('check', todos[0]);
+
+            // Confirm checked state
+            cy.get('@firstTodo').should('have.class', 'completed');
+            cy.get('@firstTodoLabel').should('contain.text', todos[0]);
+            cy.getByDataTag('todo-count')
+                .should('contain.text', `${todos.length - 1} items left`);
+            cy.get('.clear-completed').should('be.visible');
+
+            // Uncheck first todo
+            Todo.toggleTodo('uncheck', todos[0]);
+
+            // Confirm unchecked state
+            cy.get('@firstTodo').should('not.have.class', 'completed');
+            cy.get('@firstTodoLabel').should('contain.text', todos[0]);
+            cy.getByDataTag('todo-count')
+                .should('contain.text', `${todos.length} items left`);
+            cy.get('.clear-completed').should('not.exist');
+        })
+    })
 })
